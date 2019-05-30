b14076e20507ae8802b25b7ba0d04c2476bab043
Nerea Aparicio <nerea.aparicio@acuntia.dom>
2019-05-30 17:01:06 +0200

    Add route paths and modify navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
-import Header from './components/misc/Header';
+import NavBar from './components/misc/NavBar';
 import AlbumList from './components/albums/AlbumList';
 
 import './App.css';
 
 
 class App extends Component {
+
     render() {
       return (
         <div className="App">
-          <Header></Header>
-          <AlbumList></AlbumList>
+          <NavBar/>
+
+          <section className="section">
+            <div className="container">
+              <Switch>
+                <Route exact path="/albums" component={AlbumList}/>
+                {/* <Route exact path="/about" component={About} /> */}
+                {/* <Route exact path="/contacts/:id" component={FoundContact} /> */}
+                <Redirect to="/albums"/>
+              </Switch>
+            </div>
+          </section>
+
         </div>
       );
     }
---
+++ src/components/misc/NavBar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import logo from './logo.png';
+
+export default function Header (){
+
+  return (
+
+    <header>
+        <nav className="navbar navbar-expand-md navbar-light bg-light sticky-top">
+            <span className="logoimg">
+                <img className="logo rounded-pill w-50" alt="logo" src={logo}></img>
+            </span>
+
+            <div className="rest col-lg-4">
+                <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                    <div className="navbar-nav">
+                        <a href="/albums" className="nav-item nav-link toggle-active">Albums</a>
+                        <div className="nav-item dropdown">
+                            <a href="/photos" className="nav-link dropdown-toggle" data-toggle="dropdown">Photos</a>
+                            <div className="dropdown-menu">
+                                <a href="/photos" className="dropdown-item">A to Z</a>
+                                <a href="/photos" className="dropdown-item">Z to A</a>
+                            </div>
+                        </div>
+                    </div>
+                    <form className="form-inline">
+                        <div className="input-group">                    
+                            <input type="text" className="form-control" placeholder="Search"/>
+                            <div className="input-group-append">
+                                <button type="button" className="btn btn-secondary"><i className="fa fa-search"></i></button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    
+
+  )
+}
+
